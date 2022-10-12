f689a0d38749f5dbd3686c6e2d4a0026f7505421
AnTomS <thekeibee@yandex.ru>
2022-10-12 22:14:04 +0800

    Домашнее задание к занятию «2.4 Многопоточность в Android» 1) Реализовано отображение аватарок. 2)Реализованы круглые аватарки.

--- 02_threads/android/app/src/main/java/ru/netology/nmedia/dto/Post.kt
+++ 02_threads/android/app/src/main/java/ru/netology/nmedia/dto/Post.kt
@@ -1,11 +1,18 @@
 package ru.netology.nmedia.dto
 
+import ru.netology.nmedia.enumeration.AttachmentType
+
 data class Post(
     val id: Long,
     val author: String,
+    val authorAvatar: String = "",
     val content: String,
     val published: String,
     val likedByMe: Boolean,
     val likes: Int = 0,
 )
-
+data class Attachment(
+    val url: String,
+    val description: String?,
+    val type: AttachmentType,
+)
